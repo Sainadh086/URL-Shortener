61e3162f4478903bc171024359cb15d0f92c87a6
Sainadh086 <sainadh.cherukumalli@gmail.com>
2021-08-16 19:22:07 +0530

    Improving Code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 import json
 from flask import Flask, request, jsonify, render_template, redirect, url_for
@@ -7,8 +8,13 @@ from werkzeug.wrappers import response
 app = Flask(__name__)
 
 
-#checking the url present in the urls
+#checking the url present or not and sending request to cuttly api
 def check_url(url):
+    '''
+    ulr: long url sent as a request
+
+    This function takes the input from Web page and rest API. And converts the long url to short url.
+    '''
     #loading the local url file, 
     json_file = open('url.txt','r')
     json_data = json.load(json_file)
@@ -16,8 +22,8 @@ def check_url(url):
     if url in json_data.keys():
         return json_data[url]
     else:
+        #reading CUTTLY_API_KEY from environment variable
         api_key = os.environ['CUTTLY_API_KEY']
-        headers = {"Authorization": api_key}
         api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
         #sending the post request to cuttly account for converting big url to shortend url
         shorten_response =  requests.get(api_url).json()["url"]
@@ -25,26 +31,29 @@ def check_url(url):
         # runs if the status is successfull
         if shorten_response["status"] == 7:
             shorten_link = shorten_response['shortLink']
+        #writing the shortend url to local file
         json_data[url] = shorten_link
         json_file = open('url.txt','w')
         json.dump(json_data, json_file)
         json_file.close()
+        #returning the shortend url
         return shorten_link
 
 # REST API to shorten the URL
-@app.route('/short_url/', methods=['GET'])
+@app.route('/short_url/', methods=['GET', 'POST'])
 def short_url(url=None,method=None):
-    
+    '''
+    url : long url sent as a request
+    method: method for the type of  request
+
+    This function is used as a REST API to convert long url to short url.
+    '''
     # GET request
     if request.method == "GET" or method == "GET":
         url = request.args.get('url')
         shorten_link = check_url(url)
         data = {"status" : "Success", "short_url":shorten_link}
         # returning the shortend url
-        return jsonify(data)
-    else:
-        data = {"Status": "Failed", "Remarks" : "Check Credentials"}
-        # return failed status
         return jsonify(data)
 
     # runs if get request is not used
@@ -55,17 +64,25 @@ def short_url(url=None,method=None):
 # a simple webpage to use the above rest api
 @app.route("/", methods=['POST', 'GET'])
 def web_page():
+    '''
+    Function is used to render the webpage and provide the required ouput.
+    '''
+
     status = False
 
     if request.method == 'POST':
         #getting long url from the webpage
         long_url = request.form.get('long_url')
+
+        #sending the request to shorten the url
         response = short_url(url=long_url,method="GET")
+        
+        #checking the status of the response
         data = json.loads(response.get_data().decode("utf-8"))
         if data['short_url']:
             return render_template('home.html', status=True, shortend_url=shortend_url)
         else:
-            return render_template('home.html', status=True, data=data)
+            return render_template('home.html', status=True, data=data['Remarks'])
     
     return render_template('home.html', status=status)
 
