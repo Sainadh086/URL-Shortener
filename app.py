5ca462eb8ff07545849681e25d2a7b58fc2697c2
Sainadh086 <sainadh.cherukumalli@gmail.com>
2021-08-30 18:01:43 +0530

    optimizing the code

--- app.py
+++ app.py
@@ -7,60 +7,58 @@ from werkzeug.wrappers import response
 
 app = Flask(__name__)
 
+class json_check():
+    def __init__(self):
+        self.json_file = open("url.txt",'r')
+        self.json_data = json.load(self.json_file)
+        self.json_data_reverse = {v: k for k, v in self.json_data.items()}
+        self.json_file.close()
+    def get_url_status(self, url):
+        if self.json_data.get(url):
+            short_str = self.json_data[url]
+        else:
+            random_string = os.urandom(6)
+            short_str = random_string.hex().upper()
+            while self.json_data_reverse.get(short_str):
+                random_string = os.urandom(6)
+                short_str = random_string.hex().upper()
+            self.update_json(url,short_str)
+        shorten_link = "http://127.0.0.1:8080/rd/"+short_str
+        return shorten_link
+    def get_original_url(self, short_str):
+        if self.json_data_reverse.get(short_str):
+            return self.json_data_reverse[short_str]
+        else:
+            return False
+    def update_json(self,url,short_str):
+        self.json_data[url] = short_str
+        self.json_data_reverse[short_str] = url
+        json_file = open("url.txt",'w')
+        json.dump(self.json_data,json_file)
+        self.json_file.close()
+
+check_obj = json_check()
+
 
 #checking the url present or not and sending request to cuttly api
 def check_url(url):
-    '''
-    ulr: long url sent as a request
-
-    This function takes the input from Web page and rest API. And converts the long url to short url.
-    '''
-    #loading the local url file, 
-    json_file = open('url.txt','r')
-    json_data = json.load(json_file)
-    json_file.close()
-    if url in json_data.keys():
-        short_url = "http://127.0.0.1:8080/"+json_data[url]
-        return short_url
-    else:
-        #creating random string for short url
-        random_string = os.urandom(6)
-        #converting the random string to hexadecimal
-        short_str = random_string.hex().upper()
-        check_data = {y:x for x,y in json_data.items()}
-        while short_str in check_data.keys():
-            random_string = os.urandom(6)
-            short_str = random_string.hex().upper()
-        #writing the shortend url to local file
-        json_data[url] = short_str
-        json_file = open('url.txt','w')
-        json.dump(json_data, json_file)
-        json_file.close()
-        #returning the shortend url
-        shorten_link = "http://127.0.0.1:8080/rd/"+short_str
-        return shorten_link
+    #returning the shortend url
+    short_str = check_obj.get_url_status(url)
+    shorten_link = "http://127.0.0.1:8080/rd/"+short_str
+    return shorten_link
 
 #redirecting the short url to the original url
 @app.route("/rd/<short_str>", methods=['GET'])
 def url_redirect(short_str):
     '''
     Taking the Shorturl as input and redirecting to original url
-    '''
-    
-    #checking if the short url is present in the local file
-    json_file = open('url.txt','r')
-    json_data = json.load(json_file)
-    json_file.close()
+    '''  
+    long_url = check_obj.get_original_url(short_str)
 
-    #converting to (short url, long url) 
-    json_data = {y:x for x,y in json_data.items()}
-    if short_str in json_data.keys():
-        #redirecting to the original url
-        long_url = json_data[short_str]
-        return redirect(long_url, code=302)
+    if long_url:
+        return  redirect(long_url, code=302)  
     else:
-        #redirecting to the error page
-        return redirect(url_for('error_page'), code=302)
+        return "<h1> ERROR 404 URL NOT FOUND <h1>"
     
 
 
@@ -76,10 +74,14 @@ def short_url(url='',method=None):
     # GET request
     try:
         if request.method == "GET":
-            url = request.args.get('url')
+            #header = request.headers
+            #if header['Content-Type'] == 'application/json':
+                url = request.args.get('url')
+        else:
+            return jsonify({"error":"Method type must be GET"})
     except:
         pass
-    shorten_link = check_url(url)
+    shorten_link = check_obj.get_url_status(url)
     data = {"status" : "Success", "short_url":shorten_link}
     # returning the shortend url
     return jsonify(data)
@@ -101,7 +103,6 @@ def web_page():
         print("long_url :", long_url)
         #sending the request to shorten the url
         response = short_url(url=long_url,method="GET")
-        
         #checking the status of the response
         data = json.loads(response.get_data().decode("utf-8"))
         if data['short_url']:
@@ -112,4 +113,4 @@ def web_page():
     return render_template('home.html', status=status)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
